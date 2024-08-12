6f13eae160247f4f42a02fa6ec5ed7899e7d0b37
Avishkar Gawali <119069300+avishkar448@users.noreply.github.com>
2024-08-12 21:50:23 +0530

    getter-setter

--- 03_purets/dist/index.js
+++ 03_purets/dist/index.js
@@ -13,7 +13,34 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        //private _courseCount = 1
+        this._courseCount = 1;
         this.city = "pune";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1)
+            throw new Error("Course count should be more than 1");
+        this._courseCount = courseNum;
+    }
+}
+//It cannot be access the private
+// we can access protected
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const avi = new User("avishkargawali8@gmail.com", "avishkar");
--- 03_purets/src/index.ts
+++ 03_purets/src/index.ts
@@ -10,13 +10,45 @@
 // }
 
 class User {
+
+    //private _courseCount = 1
+    protected _courseCount = 1
+
     readonly city: string = "pune"
     constructor(
         public email: string,
         public name: string,
         //private userId: string 
     ) {
-        
+
+    }
+
+    private deleteToken() {
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string {
+        return `apple ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) throw new Error("Course count should be more than 1")
+
+        this._courseCount = courseNum
+    }
+
+}
+
+//It cannot be access the private
+// we can access protected
+class SubUser extends User{ 
+    isFamily: boolean=true
+    changeCourseCount(){ 
+        this._courseCount=4
     }
 }
 
